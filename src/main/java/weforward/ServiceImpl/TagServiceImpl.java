6b885a992dff7cac17d90ac3c7cb3ce3c568ed71
kk <514877036@qq.com>
2020-10-20 18:00:44 +0800

    去除了Tag中的Demand List集合，改为通过查询判断。 TagMethods中的delete和searchDemandByTagId方法会报空指针，未修正。

--- src/main/java/weforward/ServiceImpl/TagServiceImpl.java
+++ src/main/java/weforward/ServiceImpl/TagServiceImpl.java
@@ -3,7 +3,10 @@ package weforward.ServiceImpl;
 import cn.weforward.common.ResultPage;
 import cn.weforward.common.util.ResultPageHelper;
 import cn.weforward.data.persister.PersisterFactory;
+import cn.weforward.data.persister.ext.ConditionUtil;
+import weforward.Bo.Demand;
 import weforward.Bo.Tag;
+import weforward.BoImpl.DemandImpl;
 import weforward.BoImpl.TagImpl;
 import weforward.DiImpl.DemandDiImpl;
 import weforward.Exception.StatusException;
@@ -24,7 +27,7 @@ public class TagServiceImpl extends DemandDiImpl implements TagService {
     }
 
     @Override
-    public ResultPage<Tag> searchTag(String keywords) {
+    public ResultPage<Tag> searchTagByKeywords(String keywords) {
         ResultPage<? extends Tag> rp = tagPersister.startsWith("tag");
         List<Tag> list = new ArrayList<>();
         // 产品不多的时候直接遍历过滤，多了就要考虑使用索引查询
@@ -32,30 +35,51 @@ public class TagServiceImpl extends DemandDiImpl implements TagService {
             if(tag.getStatus().id == 2){
                 continue;
             }
-            tag.getName().contains(keywords);
-            list.add(tag);
-        }
-        return ResultPageHelper.toResultPage(list);
-    }
-
-    @Override
-    public Tag deleteTag(String id) throws StatusException {
-        Tag tag = tagPersister.get(id);
-        tag.deleteTag();
-        return tag;
-    }
-
-    @Override
-    public ResultPage<Tag> getTags() {
-        ResultPage<? extends Tag> rp = tagPersister.startsWith("tag");
-        List<Tag> list = new ArrayList<>();
-        // 产品不多的时候直接遍历过滤，多了就要考虑使用索引查询
-        for (Tag tag : ResultPageHelper.toForeach(rp)) {
-            if(tag.getStatus().id == 2){
+            if(!tag.getName().contains(keywords)){
                 continue;
             }
             list.add(tag);
         }
         return ResultPageHelper.toResultPage(list);
     }
+
+    @Override
+    public String deleteTag(String id) throws StatusException {
+
+        System.out.println(id);
+
+        ResultPage< ? extends Demand> rp = searchDemandByTagId(id);
+//
+//
+//        if(rp != null || rp.hasNext()){
+//            throw new StatusException("还有需求正在使用本标签，不能删除");
+//        }
+//
+//
+//        if(tagPersister.get(id).getStatus().id == 2){
+//            throw new StatusException("该标签已被删除，不能重复删除");
+//        }
+//        tagPersister.get(id).deleteTag();
+        return "删除成功";
+    }
+
+    @Override
+    public ResultPage<DemandImpl> searchDemandByTagId(String id) {
+
+        ResultPage< DemandImpl >  rp = demandPersistent.search(
+          ConditionUtil.eq(ConditionUtil.field("tag"),id)
+        );
+        
+        List<Demand> list = new ArrayList<>();
+        // 产品不多的时候直接遍历过滤，多了就要考虑使用索引查询
+        for (Demand demand : ResultPageHelper.toForeach(rp)) {
+            if(demand.getStatus().id == 999){
+                continue;
+            }
+            list.add(demand);
+        }
+        return ResultPageHelper.toResultPage(list);
+    }
+
+
 }
